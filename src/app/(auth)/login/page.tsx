c02849ff2d467e3051595333a08f9bed01e4b94b
Ravelo-Dev <zmarcos140@gmail.com>
2025-04-26 13:08:48 -0400

    Integracion

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -34,17 +34,16 @@ export default function Login() {
       }
   
       const res = await response.json();
-      localStorage.setItem('token', res.token);
+      const cleanToken = res.token.split(';')[0]; //Limpiar el token
+      localStorage.setItem('token', cleanToken);
       console.log('Inicio de sesión exitoso');
   
-      // Redirigir al dashboard
-      window.location.href = '/';
+      window.location.href = '/'; // Redirigir al dashboard
     } catch (err) {
       setError('Error de red o del servidor');
     }
   };
   
-
   return (
     <main className="h-screen flex flex-col lg:flex-row">
       <section className="h-full lg:w-[40%] grid place-content-center">
@@ -53,15 +52,13 @@ export default function Login() {
           <Form
             validationBehavior="native"
             className="flex flex-col gap-4 items-center"
-            onSubmit={async formData => {
-              await onSubmit(formData)
-            }}
+            onSubmit={onSubmit}
           >
             <Input
               name="email"
               isRequired
               type="email"
-              placeholder="Correo Electronico"
+              placeholder="Correo Electrónico"
               startContent={<UserIcon className="w-5 h-5 text-gray-600" />}
             />
             <Input
@@ -80,18 +77,19 @@ export default function Login() {
             </Button>
             {error && (
               <Card className="w-full border-red-500 border-1 bg-red-50">
-                <CardBody className="text-sm text-red-500 ">{error}</CardBody>
+                <CardBody className="text-sm text-red-500">{error}</CardBody>
               </Card>
             )}
           </Form>
         </div>
       </section>
+
       <section className="h-full grow bg-darkGreen from-teal-900 to-sky-900 grid place-content-center">
         <div className="h-full text-white flex flex-col gap-8">
           <h2 className="text-4xl font-light">¡Bienvenido de vuelta!</h2>
           <p>Optimiza tus finanzas con la IA</p>
           <p className="max-w-96 font-light">
-            Toma el control de tus ingresos y gastos inteligencia artificial. Planifica, ahorra y
+            Toma el control de tus ingresos y gastos con inteligencia artificial. Planifica, ahorra y
             haz crecer tu dinero de forma inteligente. Únete ahora y comienza a mejorar tu futuro
             financiero.
           </p>
